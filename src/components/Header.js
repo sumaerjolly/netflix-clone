b87fdb5e42bb2fb175248fc8ca6fa3455c949dcc
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-20 15:10:51 +0530

    add netflix logo and sign in button styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,52 @@
 import React from 'react';
 import logo from '../svg/logo.svg';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div className="header-container">
+    <HeaderComponent className="header-container">
       <div className="header-top">
-        <img src={logo} alt="netflix-logo" />
+        <Logo src={logo} alt="netflix-logo" />
+        <NavLink to="#" className="signIn-btn">
+          Sign In
+        </NavLink>
       </div>
-    </div>
+    </HeaderComponent>
   );
 }
 
 export default Header;
+
+// logo
+
+const Logo = styled.img`
+  width: 10rem;
+  height: 3.5rem;
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+// Header Container
+
+const HeaderComponent = styled.div`
+  .signIn-btn {
+    right: 0;
+    margin: 1.125rem 3% 0;
+    padding: 0.4375rem 1.0625rem;
+    font-weight: 400;
+    line-height: normal;
+    border-radius: 0.1875rem;
+    font-size: 1rem;
+    background: var(--main-red);
+    position: absolute;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+    transition: background 0.2s ease-in;
+    &:hover {
+      background: var(--main-red-hover);
+    }
+  }
+`;
