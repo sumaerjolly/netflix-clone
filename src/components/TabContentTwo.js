af96aa6f780b741328b186a79a6a68b4daa64c45
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-21 19:01:52 +0530

    add tab two full styling and text

--- src/components/TabContentTwo.js
+++ src/components/TabContentTwo.js
@@ -15,9 +15,9 @@ function TabContentTwo() {
           </span>
           <Button className="btn">try it now</Button>
         </div>
-        <div className="tab-bottom-cotent">
+        <div className="tab-bottom-content">
           <div>
-            <img src={Img1} alt="Tv photo" />
+            <img src={Img1} alt="Tv" style={{ width: '18.75rem' }} />
             <h3>Watch on your TV</h3>
             <p>
               Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
@@ -25,20 +25,26 @@ function TabContentTwo() {
             </p>
           </div>
           <div>
-            <img src={Img2} alt="Tv photo" />
-            <h3>Watch on your TV</h3>
-            <p>
-              Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
-              players and more.{' '}
-            </p>
+            <img
+              src={Img2}
+              alt="Tablet "
+              style={{ paddingTop: '0.625rem', width: '18.75rem' }}
+            />
+            <h3>Watch instantly or download later</h3>
+            <p>Available on phone and tablet, wherever you go.</p>
           </div>
           <div>
-            <img src={Img3} alt="Tv photo" />
-            <h3>Watch on your TV</h3>
-            <p>
-              Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
-              players and more.{' '}
-            </p>
+            <img
+              src={Img3}
+              alt="Macbook"
+              style={{
+                width: '18.75rem',
+                paddingTop: '0.625rem',
+                paddingBottom: '0.625rem'
+              }}
+            />
+            <h3>Use any computer</h3>
+            <p>Watch right on Netflix.com</p>
           </div>
         </div>
       </div>
@@ -74,5 +80,23 @@ const TabContentContainer = styled.div`
   .btn {
     // margin: 0 1.25rem 1.25rem;
     grid-column: 10/12;
+  }
+
+  // Tab Bottom Content
+
+  .tab-bottom-content {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem;
+    text-align: center;
+    margin-top: 2rem;
+  }
+
+  h3 {
+    margin: 0.5rem 0;
+  }
+
+  p {
+    color: var(--main-grey);
   }
 `;
