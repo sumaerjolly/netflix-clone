9e5bf99ec08083992e19fe0b5e92e505b6f608c7
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-22 12:37:29 +0530

    add footer text and basic react layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import TabComponent from '../components/TabComponent';
+import Footer from '../components/Footer';
 
 function Main() {
   return (
     <div>
       <Header />
       <TabComponent />
+      <Footer />
     </div>
   );
 }
