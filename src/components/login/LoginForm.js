69f85c23e3290357b343853976e96505f06fa9e5
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-23 14:23:34 +0530

    add remember me,checkbox and need help to form with styling

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 function LoginForm() {
   return (
@@ -16,8 +17,16 @@ function LoginForm() {
             <label>Password</label>
           </div>
           <div className="input-container">
-            <a type="submit">Sign In</a>
+            <Button type="submit">Sign In</Button>
           </div>
+          <label className="checkbox-container">
+            Remember Me
+            <input type="checkbox" checked />
+            <span className="checkmark"></span>
+          </label>
+          <Link to="/" className="need-help">
+            Need Help?
+          </Link>
         </form>
       </div>
     </FormContainer>
@@ -75,4 +84,68 @@ const FormContainer = styled.div`
     top: 0.4375rem;
     font-size: 0.7rem;
   }
+
+  input:focus {
+    outline: none;
+  }
+
+  // checkbox
+
+  .checkbox-container {
+    margin-left: 0.75rem;
+    padding-left: 1.875rem;
+    position: relative;
+    font-size: 0.9rem;
+    cursor: pointer;
+  }
+
+  .checkbox-container input {
+    display: none;
+  }
+
+  .checkbox-container .checkmark {
+    display: inline-block;
+    background: #454545;
+    width: 1.1rem;
+    height: 1.1rem;
+    left: 0;
+    top: 0;
+    border-radius: 0.1rem;
+    position: absolute;
+  }
+
+  .checkbox-container input:checked + .checkmark:after {
+    content: '';
+    position: absolute;
+    height: 0.25rem;
+    width: 0.625rem;
+    border-left: 2px solid #000;
+    border-bottom: 2px solid #000;
+    top: 25%;
+    left: 21%;
+    transform: rotate(-45deg);
+  }
+
+  .need-help {
+    text-decoration: none;
+    color: #828282;
+    margin-left: 6.6rem;
+    font-size: 0.9rem;
+  }
+`;
+
+const Button = styled.button`
+  color: #fff;
+  background: rgba(229, 9, 20);
+  border: none;
+  outline: none;
+  padding: 0.8rem 1.3rem;
+  border-radius: 0.125rem;
+  font-size: 1rem;
+  text-align: center;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
+  transition: opacity 200ms ease-in;
+  cursor: pointer;
+  text-decoration: none;
+  margin: 1rem 0;
 `;
