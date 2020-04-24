934f1fa949588396908c3c8e2a5932a8a79a0e13
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-24 15:06:38 +0530

    add header content and some basic styling:

--- src/pages/ChoosePlan.js
+++ src/pages/ChoosePlan.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../svg/logo.svg';
+import Checkmark from '../images/Checkmark.png';
 import { Link, NavLink } from 'react-router-dom';
 
 function ChoosePlan() {
@@ -14,6 +15,13 @@ function ChoosePlan() {
           Sign In
         </NavLink>
       </div>
+      <div className="header-content">
+        <img classname="checkmark-logo" src={Checkmark} alt="checkmark" />
+        <p>
+          Step <strong>1</strong> of <strong>3</strong>
+        </p>
+        <h2>Choose your plan.</h2>
+      </div>
     </MainContainer>
   );
 }
@@ -22,4 +30,28 @@ export default ChoosePlan;
 
 // Main Container
 
-const MainContainer = styled.div``;
+const MainContainer = styled.div`
+  // header top styling
+  background: #fff;
+  .header-top {
+    background: #fff;
+    height: 6rem;
+    border-bottom: solid 1px #6e6e6e;
+  }
+
+  .header-content {
+    display: grid;
+    justify-content: center;
+    background: #fff;
+    color: var(--main-dark);
+    margin-bottom: 6rem;
+    width: 65%;
+    position: relative;
+    margin: auto;
+    margin-top: 4.5rem;
+    text-align: center;
+    align-content: center;
+    flex-direction: column;
+    z-index: 2;
+  }
+`;
