47b72bea8e6da5985fa89aad46a1f5d616c15538
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-24 14:22:27 +0530

    add basic set up of chooseplan page

---
+++ src/pages/ChoosePlan.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../svg/logo.svg';
+import { Link, NavLink } from 'react-router-dom';
+
+function ChoosePlan() {
+  return <MainContainer></MainContainer>;
+}
+
+export default ChoosePlan;
+
+// Main Container
+
+const MainContainer = styled.div``;
