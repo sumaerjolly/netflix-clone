d4cb35165f6f988b2e6ce892318e8a67eca8ee1f
Sumaer Jolly <sumaerjolly@gmail.com>
2020-04-24 14:31:41 +0530

    add routes and logo to choose plan page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Main from './pages';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
+import ChoosePlan from './pages/ChoosePlan';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Main} />
       <Route path="/login" component={Login} />
+      <Route path="/plans" component={ChoosePlan} />
     </Switch>
   );
 }
--- src/pages/ChoosePlan.js
+++ src/pages/ChoosePlan.js
@@ -4,7 +4,18 @@ import logo from '../svg/logo.svg';
 import { Link, NavLink } from 'react-router-dom';
 
 function ChoosePlan() {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <div className="header-top">
+        <Link to="/">
+          <img src={logo} alt="netflix logo" />
+        </Link>
+        <NavLink to="/login" className="btn sign-in-btn">
+          Sign In
+        </NavLink>
+      </div>
+    </MainContainer>
+  );
 }
 
 export default ChoosePlan;
